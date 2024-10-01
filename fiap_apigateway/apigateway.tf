1cc21acbc6d4fb23d8c433fe5993e170ae294583
PedroGimenezSilva <pedro.msilva@bandtec.com.br>
2024-10-01 12:36:16 -0300

    fixing apu-gateway config

--- fiap_apigateway/apigateway.tf
+++ fiap_apigateway/apigateway.tf
@@ -3,21 +3,18 @@ resource "aws_apigatewayv2_api" "fiap_apigateway" {
   protocol_type = "HTTP"
 }
 
-resource "aws_apigatewayv2_integration" "fiap_apigateway_integration" {
-  api_id           = aws_apigatewayv2_api.fiap_apigateway.id
-  integration_type = "HTTP_PROXY"
-  integration_uri  = "http://teste"
-}
-
-resource "aws_apigatewayv2_route" "fiap_apigateway_integration" {
-  api_id    = aws_apigatewayv2_api.fiap_apigateway.id
-  route_key = "ANY /{proxy+}"
-  target    = "integrations/${aws_apigatewayv2_integration.fiap_apigateway_integration.id}"
+resource "aws_apigatewayv2_vpc_link" "loadbalancer-vpc-link" {
+  name               = "loadbalancer-vpc-link"
+  subnet_ids         = ["subnet-0754183aca138c7aa", "subnet-0115b6d08158577bf"]
+  security_group_ids = ["sg-0aa865e43167f6313"]
+  tags = {
+    Name = "loadbalancer-vpc-link"
+  }
 }
 
 resource "aws_apigatewayv2_stage" "fiap_apigateway_stage" {
   api_id      = aws_apigatewayv2_api.fiap_apigateway.id
-  name        = "prod"
+  name        = "$default"
   auto_deploy = true
 }
 
